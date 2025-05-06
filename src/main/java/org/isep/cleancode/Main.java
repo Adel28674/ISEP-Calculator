584f2eefc731e0e5cf454c64f6192cc88aff0236
ahammiche <adelhammiche.pro@gmail.com>
2025-05-06 18:39:03 +0200

    cleaning

--- src/main/java/org/isep/cleancode/Main.java
+++ src/main/java/org/isep/cleancode/Main.java
@@ -5,8 +5,5 @@ import org.isep.cleancode.calculator.Calculator;
 public class Main {
     public static void main(String[] args) {
 
-        Calculator c = new Calculator();
-        System.out.println(c.substract("12 - 12"));
-
     }
 }
